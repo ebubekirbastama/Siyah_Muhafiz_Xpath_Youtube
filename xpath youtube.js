1ac081873034692e244960a38accea5f580b84a5
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2019-09-19 01:45:11 +0300

    Add files via upload

---
+++ xpath youtube.js
@@ -0,0 +1,18 @@
+//Siyah Muhafiz Web Browser Console XPath Kullanımı Youtube\\
+//-------------------------------------------------\\
+                   //Ham Hali Kullanımı\\
+//--------------------------------------------------\\
+//Örnek:
+//-------------------------------------------------------
+//$x("//a[@class='yt-simple-endpoint style-scope ytd-grid-video-renderer']")[0].getAttribute('href');
+
+
+var syhmhz = new Array();
+for (var i = 0; i < $x("//a[@class='yt-simple-endpoint style-scope ytd-grid-video-renderer']").length; i++) 
+{
+  syhmhz.push($x("//a[@class='yt-simple-endpoint style-scope ytd-grid-video-renderer']")[i].getAttribute('href'));
+}
+ for (var j = 0; j < syhmhz.length; j++) 
+{
+  document.write("https://www.youtube.com"+syhmhz[j]+"<br>");
+}
